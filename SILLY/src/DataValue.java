11e8a6493296c05dbf4b950dc2f2e92ba6631f16
Caleb C <118626040+ccannon062@users.noreply.github.com>
2025-02-26 10:05:54 -0600

    Add files via upload

---
+++ SILLY/src/DataValue.java
@@ -0,0 +1,12 @@
+/**
+ * Interface that defines the data types for the SILLY language. 
+ *   @author Dave Reed modified by Caleb Cannon
+ *   @version 1/20/25
+ */
+public interface DataValue extends Comparable<DataValue> {   
+    public static enum Type { NUMBER, BOOLEAN, LIST, CHAR }
+    
+    public Object getValue();
+    public DataValue.Type getType();
+    public String toString(); 
+}
